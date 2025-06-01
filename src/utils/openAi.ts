8a6973ff55ef41e5300d710285bed840f5f2eb5d
bavithra-pyt <bavithra.r@pickyourtrail.com>
2025-06-01 11:48:23 +0530

    feat: removed open api key to env file

--- src/utils/openAi.ts
+++ src/utils/openAi.ts
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
-import { gptKey } from "./constant";
-
+// import { gptKey } from "./constant";
+console.log(process.env.NEXT_PUBLIC_GPT_KEY, "key");
 export const client = new OpenAI({
-  apiKey: gptKey,
+  apiKey: process.env.NEXT_PUBLIC_GPT_KEY,
   dangerouslyAllowBrowser: true,
 });
